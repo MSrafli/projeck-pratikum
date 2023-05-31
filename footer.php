9277f8d836ed81e12528c410fd1af40ca16844ca
MSrafli <tidakadaakun09@gmail.com>
2023-05-31 11:38:58 +0800

    selesai pertemuan 3

---
+++ footer.php
@@ -0,0 +1,13 @@
+</div>
+<footer>
+    <div class="container">
+        <span>&copy; <?php echo date('Y'); ?> - FTI UNISKA</span>
+    </div>
+</footer>
+
+<script src="js/jquery-3.5.1.slim.min"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ profile.php
@@ -0,0 +1,54 @@
+<?php include "header.php" ?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="text-center">
+            <img src="img/rafli.jpg" alt="mochamad surgani rafli" width="160" class="rounded-circle">
+            <table class="table mt-4" style="width:100%; margin:0 auto;">
+                <tbody>
+                    <tr>
+                        <td align="right" width="50%"><strong>Npm :</strong></td>
+                        <td align="left" width="50%">2110010133</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Fakultas / Program Studio :</strong></td>
+                        <td align="left" width="50%">Teknologi Informasi / Teknik Informatika</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Nama Lengkap :</strong></td>
+                        <td align="left" width="50%">Mochamad surgani rafli</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Tempat, Tanggal Lahir :</strong></td>
+                        <td align="left" width="50%">tuban, 1 september 2001</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Telepon :</strong></td>
+                        <td align="left" width="50%">082255008786</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>email :</strong></td>
+                        <td align="left" width="50%">tidakadaakun09@gmail.com</td>
+                    </tr>
+                    <tr>
+                        <td align="right" width="50%"><strong>Media Sosial :</strong></td>
+                        <td align="left" width="50%">
+                            <a href="http://facebook.com/mdedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/medsos/facebook.svg" width="30" alt="facebook">
+                            </a>
+                            <a href="http://instagram.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img/ig.svg" width="30" alt="instagram">
+                            </a>
+                            <a href="http://twitter.com/dedyrosyadi" target="_blank" style="text-decoration:none;">
+                                <img src="img//twitter.svg" width="30" alt="twitter">
+                            </a>
+                            <a href="http://linkedin.com/in/dedyrosyadi" target="_blank">
+                                <img src="img/medsos/linkedin.svg" width="30" alt="linkedin">
+                            </a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+<?php include "footer.php" ?>
